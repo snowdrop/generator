1d88e79ce054fab97c5d76ec67aa2f2c500ff389
Georgios Andrianakis <geoand@gmail.com>
2018-08-30 12:04:39 +0300

    Remove useless code block from support script

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -8,19 +8,6 @@ APP="generator"
 
 mkdir -p $RELEASE_DIR
 
-# if this is run on travis make sure that binary was build with corrent version
-if [[ -n $TRAVIS_TAG ]]; then
-    echo "Checking if app's version was set to the same version as current tag"
-    # use sed to get only semver part
-    bin_version=$(${BIN_DIR}/linux-amd64/sb version | head -1 | sed "s/^sb \(.*\) (.*)$/\1/")
-    if [ "$TRAVIS_TAG" == "${bin_version}" ]; then
-        echo "OK: app's  version output is matching current tag"
-    else
-        echo "ERR: TRAVIS_TAG ($TRAVIS_TAG) is not matching 'sb version' (v${bin_version})"
-        exit 1
-    fi
-fi
-
 # gziped binaries
 for arch in `ls -1 $BIN_DIR/`;do
     suffix=""
