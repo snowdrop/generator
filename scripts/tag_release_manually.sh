e7bb648e162b4c01bd48783a445821ebfbb9e60e
Charles Moulliard <cmoulliard@redhat.com>
2018-08-31 11:19:04 +0200

    Rename  var to 694a52a49a67

--- scripts/tag_release_manually.sh
+++ scripts/tag_release_manually.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-TAG_ID=$1
+TAG_ID="release-"$1
 GIT_TOKEN=$2
 
 echo "Tagging ..."
-git tag -a $TAG -m "$TAG release"
+git tag -a $TAG_ID -m "$TAG_ID"
 
 echo "Releasing $TAG_ID ..."
-JSON='{"tag_name": "'"$TAG"'","target_commitish": "master","name": "'"$TAG"'","body": "'"$TAG"'-release","draft": false,"prerelease": false}'
+JSON='{"tag_name": "'"$TAG_ID"'","target_commitish": "master","name": "'"$TAG_ID"'","body": "'"$TAG_ID"'-release","draft": false,"prerelease": false}'
 curl -H "$AUTH" \
     -H "Content-Type: application/json" \
     -d "$JSON" \
