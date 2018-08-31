8af7579b02e4479ed0f518a964d228d7952a09af
Charles Moulliard <cmoulliard@redhat.com>
2018-08-31 11:38:56 +0200

    Add missing vars to define the URL of the github API server

--- scripts/tag_release_manually.sh
+++ scripts/tag_release_manually.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 TAG_ID="release-"$1
-GIT_TOKEN=$2
+GITHUB_API_TOKEN=$2
+
+OWNER="snowdrop"
+REPO="generator"
+
+AUTH="Authorization: token $GITHUB_API_TOKEN"
+
+GH_API="https://api.github.com"
+GH_REPO="$GH_API/repos/$OWNER/$REPO"
 
 echo "Tagging ..."
-git tag -a $TAG_ID -m "$TAG_ID"
+#git tag -a $TAG_ID -m "$TAG_ID"
 
 echo "Releasing $TAG_ID ..."
-JSON='{"tag_name": "'"$TAG_ID"'","target_commitish": "master","name": "'"$TAG_ID"'","body": "'"$TAG_ID"'-release","draft": false,"prerelease": false}'
-curl -H "$AUTH" \
-    -H "Content-Type: application/json" \
-    -d "$JSON" \
-    $GH_REPO/releases
+JSON='{"tag_name": "'"$TAG_ID"'","target_commitish": "master","name": "'"$TAG_ID"'","body": "'"$TAG_ID"'","draft": false,"prerelease": false}'
+#curl -H "$AUTH" \
+#    -H "Content-Type: application/json" \
+#    -d "$JSON" \
+#    $GH_REPO/releases
+
+echo curl -H "$AUTH" \
+     -H "Content-Type: application/json" \
+     -d "$JSON" \
+     $GH_REPO/releases
