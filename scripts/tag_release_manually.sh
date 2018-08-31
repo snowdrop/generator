4d37788e214d336bf8f69d1e1cfea8d8b4b438ad
Charles Moulliard <cmoulliard@redhat.com>
2018-08-31 11:39:43 +0200

    Remove comments to be able to git tag and do curl

--- scripts/tag_release_manually.sh
+++ scripts/tag_release_manually.sh
@@ -5,23 +5,16 @@ GITHUB_API_TOKEN=$2
 
 OWNER="snowdrop"
 REPO="generator"
-
 AUTH="Authorization: token $GITHUB_API_TOKEN"
-
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/$OWNER/$REPO"
 
 echo "Tagging ..."
-#git tag -a $TAG_ID -m "$TAG_ID"
+git tag -a $TAG_ID -m "$TAG_ID"
 
 echo "Releasing $TAG_ID ..."
 JSON='{"tag_name": "'"$TAG_ID"'","target_commitish": "master","name": "'"$TAG_ID"'","body": "'"$TAG_ID"'","draft": false,"prerelease": false}'
-#curl -H "$AUTH" \
-#    -H "Content-Type: application/json" \
-#    -d "$JSON" \
-#    $GH_REPO/releases
-
-echo curl -H "$AUTH" \
-     -H "Content-Type: application/json" \
-     -d "$JSON" \
-     $GH_REPO/releases
+curl -H "$AUTH" \
+    -H "Content-Type: application/json" \
+    -d "$JSON" \
+    $GH_REPO/releases
