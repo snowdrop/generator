1c45fddc1ad42f3167690d5c6aff60f3c544b077
Charles Moulliard <cmoulliard@redhat.com>
2018-09-20 16:56:06 +0200

    Move code under dummy folder

---
+++ pkg/template/tmpl/jsp/src/main/java/dummy/MvcApplication.java
@@ -0,0 +1,19 @@
+package {{.PackageName}};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MvcApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MvcApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MvcApplication.class, args);
+	}
+}
