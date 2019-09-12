74ea74905a81585a846868a70a6ddf90d9e64b11
aurea munoz <amunozhe@redhat.com>
2019-09-12 12:11:26 +0200

    chore: add make assets task in tag_release_script

--- scripts/tag_release_manually.sh
+++ scripts/tag_release_manually.sh
@@ -9,10 +9,22 @@ AUTH="Authorization: token $GITHUB_API_TOKEN"
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/$OWNER/$REPO"
 
-echo "Tagging ..."
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Compiling go templates${NC}"
+make assets
+
+echo -e "${GREEN}Commit assets >> git commit -m 'fix: re-generate assets'${NC}"
+git add pkg/template/assets_vfsdata.go
+git commit -m "fix: re-generate assets" pkg/template/assets_vfsdata.go
+echo -e "${GREEN}Pushing assets to master${NC}"
+git push origin master
+
+echo -e "${GREEN}Tagging ...${NC}"
 git tag -a $TAG_ID -m "$TAG_ID"
 
-echo "Releasing $TAG_ID ..."
+echo -e "${GREEN}Releasing $TAG_ID ...${NC}"
 JSON='{"tag_name": "'"$TAG_ID"'","target_commitish": "master","name": "'"$TAG_ID"'","body": "'"$TAG_ID"'","draft": false,"prerelease": false}'
 curl -H "$AUTH" \
     -H "Content-Type: application/json" \
